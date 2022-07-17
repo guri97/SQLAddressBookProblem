1c5323d85950a6efcf9fb07ec7451780af57dad8
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-17 19:59:38 +0530

    [gurpreet] UC3_InsertContact

--- SQLAddressBookProblem/SQLAddressBookProblem/SQLQuery1.sql
+++ SQLAddressBookProblem/SQLAddressBookProblem/SQLQuery1.sql
@@ -12,3 +12,11 @@ PhoneNumber bigint,
 Email varchar(200)
 
 );
+----------------------UC-3----------------------
+--insert contact details into table--
+insert into AddressBook(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email)
+values('Gurpreet Singh','Port Blair','Andaman And Nicobar Islands',744101,953182464,'gsgurpreet766@gmail.com'),
+('Chinna','Gurgaon','New Delhi',110001,6239458567,'chinna5869@gmail.com'),
+('Vivek','Greater Noida','New Delhi',110001,9645065768,'Vivek5868@gmail.com'),
+('Gayatri','Port Blair','Andaman And Nicobar Islands',744105,7892100876,'gayatri5869@gmail.com');
+select * from AddressBook;
