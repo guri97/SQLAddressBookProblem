17dd8c65033a1899386fcb1142421ab1f267350b
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-17 19:44:21 +0530

    [gurpreet] UC2_CreatingTheTable

--- SQLAddressBookProblem/SQLAddressBookProblem/SQLQuery1.sql
+++ SQLAddressBookProblem/SQLAddressBookProblem/SQLQuery1.sql
@@ -1,2 +1,14 @@
 ------------UC1 CREATE DATABASE-------------
 CREATE DATABASE ADDRESSBOOK_SERVICES;
+-------UC2-create table for addressbook servoces -----------
+CREATE TABLE AddressBook(
+FirstName varchar(200),
+LastName varchar(200),
+Address varchar(1000),
+City char(100),
+State char(100),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(200)
+
+);
